f2758d47e752345b5860218b7a18014239a24f26
Dfulop98 <dobo.fulop@icloud.com>
2023-08-12 16:07:16 +0200

    Fix: change restore method

---
+++ babilon_demo/restore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chown git.git /var/opt/gitlab/backups/1691848362_2023_08_12_16.2.3_gitlab_backup.tar
+gitlab-ctl stop unicorn
+gitlab-ctl stop puma
+gitlab-ctl stop sidekiq
+gitlab-backup restore BACKUP=1691848362_2023_08_12_16.2.3
+gitlab-ctl start unicorn
+gitlab-ctl start puma
+gitlab-ctl start sidekiq
+gitlab-ctl reconfigure
+gitlab-ctl restart
+gitlab-rake gitlab:check SANITIZE=true
