afc17f15c0eebd2459d027cf3fa2bb489a910992
Dfulop98 <dobo.fulop@icloud.com>
2023-08-13 14:25:18 +0200

    Fix: restore incorrect chown

--- babilon_demo/restore.sh
+++ babilon_demo/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chown git.git /etc/gitlab/config_backup/gitlab-etc-backup2.tar.gz
 tar -xvf /etc/gitlab/config_backup/gitlab-etc-backup2.tar.gz -C /etc/gitlab
-chown git.git /var/opt/gitlab/backups/1691848362_2023_08_12_16.2.3_gitlab_backup.tar
+chown git.git /var/opt/gitlab/backups/1691922774_2023_08_13_16.2.3_gitlab_backup.tar
 gitlab-ctl stop unicorn
 gitlab-ctl stop puma
 gitlab-ctl stop sidekiq
