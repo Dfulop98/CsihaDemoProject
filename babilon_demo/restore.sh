04445667eb46d02ca75b0eab30c0b9d81416cb80
Dfulop98 <dobo.fulop@icloud.com>
2023-08-13 13:58:44 +0200

    Fix: restore error

--- babilon_demo/restore.sh
+++ babilon_demo/restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chown git.git /etc/gitlab/gitlab-etc-backup.tar.gz
-tar -xvf /etc/gitlab/gitlab-etc-backup.tar.gz -C /etc/gitlab
+chown git.git /etc/gitlab/config_backup/gitlab-etc-backup2.tar.gz
+tar -xvf /etc/gitlab/config_backup/gitlab-etc-backup2.tar.gz -C /etc/gitlab
 chown git.git /var/opt/gitlab/backups/1691848362_2023_08_12_16.2.3_gitlab_backup.tar
 gitlab-ctl stop unicorn
 gitlab-ctl stop puma
